e6b8cccc88964b7e29dd16fe8602e942e7b50a3b
oktayekincioglu <oktayekincioglu@gmail.com>
2022-02-14 19:29:32 +0000

    Example app

---
+++ inventory/inventory-api/src/main/java/uk/co/codelity/inventory/api/controller/InventoryController.java
@@ -0,0 +1,8 @@
+package uk.co.codelity.inventory.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface InventoryController {
+
+}
---
+++ inventory/inventory-service/src/main/java/uk/co/codelity/inventory/service/controller/InventoryControllerImpl.java
@@ -0,0 +1,8 @@
+package uk.co.codelity.inventory.service.controller;
+
+import uk.co.codelity.inventory.api.controller.InventoryController;
+
+
+public class InventoryControllerImpl implements InventoryController {
+
+}
